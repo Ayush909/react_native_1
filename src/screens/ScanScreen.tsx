da396c4ea33bcf11fb466052329622809f14f977
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2025-03-11 00:23:14 +0530

    adding camera

--- src/screens/ScanScreen.tsx
+++ src/screens/ScanScreen.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 // import Camera from '../components/Camera';
 // import {useNavigation} from '@react-navigation/native';
-
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from 'react-native-vision-camera';
 const ScanScreen = () => {
   // const navigation = useNavigation();
 
@@ -11,10 +15,35 @@ const ScanScreen = () => {
   //   navigation.navigate('ResultScreen', {imageUri});
   // };
 
+  const device = useCameraDevice('back');
+  const hasPermission = useCameraPermission();
+
+  useEffect(() => {
+    checkPermission();
+  }, []);
+
+  const checkPermission = async () => {
+    const status = await Camera.requestCameraPermission();
+    console.log('Camera permission status:', status);
+  };
+
+  if (!hasPermission) {
+    return <Text>No camera permission</Text>;
+  }
+
+  if (!device) {
+    return <Text>No camera device</Text>;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Scan your receipt</Text>
       {/* <Camera onCapture={handleCapture} /> */}
+      <Camera
+        style={{flex: 1, width: '100%'}}
+        device={device}
+        isActive={true}
+      />
     </View>
   );
 };
